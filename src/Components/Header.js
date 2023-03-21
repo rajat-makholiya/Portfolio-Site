3eb9f2ebce766344de6c4030f93c3a144c1083aa
rajat-makholiya <rajat.makholiya@goapptiv.in>
2023-03-21 17:07:07 +0530

    Header Component Structured

---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import Stack from "@mui/material";
+import PersonalLogo from "../assets/Images";
+
+const Header = () => {
+  return (
+    <>
+      <Stack>
+        <PersonalLogo />
+      </Stack>
+    </>
+  );
+};
+
+export default Header;
