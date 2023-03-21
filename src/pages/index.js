5ae73a2bb565c4b9a16ab9d3b9dec8e73418fc18
rajat-makholiya <rajat.makholiya@goapptiv.in>
2023-03-21 17:08:00 +0530

    Linked Index File to Main Profile Page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
-import * as React from "react"
+import * as React from "react";
+import Profile from "./Profile";
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <Profile />;
 }
