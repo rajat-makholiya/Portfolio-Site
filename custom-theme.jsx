fcd8c8c7648b381d82c03ba03b344046d7f968d7
rajat-makholiya <rajat.makholiya@goapptiv.in>
2023-03-30 16:37:07 +0530

    Custom Theme File

---
+++ custom-theme.jsx
@@ -0,0 +1,18 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    h1: {
+      fontFamily: "Knewave",
+      fontWeight: 500,
+    },
+    subtitle1: {
+      fontFamily: "Segoe UI",
+    },
+    h5: {
+      fontFamily: "Segoe UI",
+    },
+  },
+});
+
+export default theme;
